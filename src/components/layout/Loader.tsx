ae64eb3f2c1a24077dd423898b0a5449772df5fa
shumman <shumman2007@hotmail.com>
2024-06-10 12:18:58 +0500

    pushing files

--- src/components/layout/Loader.tsx
+++ src/components/layout/Loader.tsx
@@ -19,16 +19,13 @@ export default function Loader() {
                 loaderRef.current!.style.transform = `translateX(${count}%)`
                 count+=1
             },5)
-            // document.getElementsByTagName('body')[0].style.overflowX = 'hidden'
-            // document.getElementsByTagName('body')[0].style.overflowY = 'visible'
-            // loaderRef.current!.style.transform ='translateX(100%)'
         },2000)
     },[])
     return (
         <>
             <div ref={loaderRef} className=" loader absolute flex z-[2222222222222222] justify-center items-center bg-black w-full h-full top-0">
                 <div className="h-1/2 w-1/2 flex justify-center">
-                    <Image className="w-1/2 h-full animate-bounce" src={'/images/loader/loader.jpg'} alt="loader" width={300} height={400}/>
+                    <Image className="w-1/2 h-full animate-bounce" priority src={'/images/loader/loader.jpg'} alt="loader" width={300} height={400}/>
                 </div>
             </div>
         </>
